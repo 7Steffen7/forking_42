9948752bc4946456acfb01e5add457e1e1326af1
Steffen Parth <steffen.parth@icloud.com>
2024-12-11 14:21:31 +0100

    working

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef char i8;
 typedef unsigned char u8;
@@ -55,6 +56,39 @@ struct file_content   read_entire_file(char* filename)
 	return (struct file_content){file_data, file_size};
 }
 
+void print_message(u8 *pixel_data, int message_len, int y, int start_x, int end_x, u32 row_size) {
+	printf("start to end %d - %d\n", start_x * 4, end_x * 4);
+	int nbr_of_lines = message_len / (end_x * 4 - start_x * 4);
+	printf("nbr_of_lines: %d\n", nbr_of_lines);
+
+	while (message_len)
+	{
+		int x = start_x;
+		while (x <= end_x) {
+			u32 pixel_index = (y * row_size) + (x * 4);
+			// printf("pixel_idx: %d", pixel_index);
+			if (message_len) {
+				u8 blue		= pixel_data[pixel_index + 0];
+				write(1, &blue, 1);
+				message_len--;
+			} if (message_len) {
+				u8 green	= pixel_data[pixel_index + 1];
+				write(1, &green, 1);
+				message_len--;
+			} if (message_len) {
+				u8 red		= pixel_data[pixel_index + 2];
+				write(1, &red, 1);
+				message_len--;
+			}
+			// message_len--;
+
+			x++;
+		}
+		y--;
+	}
+
+}
+
 int main(int argc, char** argv)
 {
 	if (argc != 2)
@@ -69,6 +103,50 @@ int main(int argc, char** argv)
 		return 1;
 	}
 	struct bmp_header* header = (struct bmp_header*) file_content.data;
-	printf("signature: %.2s\nfile_size: %u\ndata_offset: %u\ninfo_header_size: %u\nwidth: %u\nheight: %u\nplanes: %i\nbit_per_px: %i\ncompression_type: %u\ncompression_size: %u\n", header->signature, header->file_size, header->data_offset, header->info_header_size, header->width, header->height, header->number_of_planes, header->bit_per_pixel, header->compression_type, header->compressed_image_size);
+	u8	*pixel_data = (u8 *)(file_content.data + header->data_offset);
+
+	u32	found_header = 0;
+	u8	header_line = 0;
+	u32 message_len = 0;
+	u32 message_x = 0;
+	for (u32 y = 0; y < header->height; y++) {
+	    for (u32 x = 0; x < header->width; x++) {
+			if (message_x != 0) {
+				x = message_x - message_len + 1;
+			}
+	        u32 row_size = ((header->width * header->bit_per_pixel + 31) / 32) * 4;
+
+			u32 pixel_index = (y * row_size) + (x * 4);
+
+			u8 blue		= pixel_data[pixel_index + 0];
+			u8 green	= pixel_data[pixel_index + 1];
+			u8 red		= pixel_data[pixel_index + 2];
+			// u8 padding	= pixel_data[pixel_index + 3];
+
+			if (message_x > 0) {
+
+			}
+
+			if (blue == 127 &&  green == 188 && red == 217 && found_header == 0) {
+				printf("Pixel (%u,%u): B=%u, G=%u, R=%u\n", y, x, blue, green, red);
+				found_header = x;
+			}
+			else if (blue == 127 &&  green == 188 && red == 217 && x > found_header) {
+				printf("Pixel (%u,%u): B=%u, G=%u, R=%u\n", y, x, blue, green, red);
+				header_line = 1;
+			} else if (!(blue == 127 &&  green == 188 && red) && header_line == 1 ) {
+				message_len = blue + red;
+				printf("Pixel (%u,%u): B=%u, G=%u, R=%u\n", y, x, blue, green, red);
+				printf("message_len: %d\n", message_len);
+				print_message(pixel_data, message_len, y -= 2 , found_header + 2, x, row_size);
+				return(0);
+				// header_line = 2;
+				// message_x = x;
+				// y -= 3;
+				// break;
+			}
+	    }
+	}
+	// printf("signature: %.2s\nfile_size: %u\ndata_offset: %u\ninfo_header_size: %u\nwidth: %u\nheight: %u\nplanes: %i\nbit_per_px: %i\ncompression_type: %u\ncompression_size: %u\n", header->signature, header->file_size, header->data_offset, header->info_header_size, header->width, header->height, header->number_of_planes, header->bit_per_pixel, header->compression_type, header->compressed_image_size);
 	return 0;
 }
